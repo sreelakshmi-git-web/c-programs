0ccac979d9b177034ade008038ba3906d631e7d4
sreelakshmi-git-web <sreelakshmik783@gmail.com>
2026-03-08 10:06:10 +0530

    Add files via upload

---
+++ FIRSTT.C
@@ -0,0 +1,14 @@
+int main(void)
+{
+//comment
+int num;
+float amount;
+getch();
+num=(5+5);
+amount=(2.5*1.0);
+printf("%d\n",num);
+printf("%f\n",amount);
+printf("i see,\nI REMEMBER!");
+getch();
+return 0;
+}
